d2887b6bad7ad12a1da19e5208af64931158162a
yasui katsuhide <>
2025-08-02 16:09:19 +0900

    tailwindの導入

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import './assets/main.css'
-
+import './assets/tailwind.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
