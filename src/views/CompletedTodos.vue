8fb408096993f3d43294097c06db4eeffd3177af
yasui katsuhide <>
2025-08-08 05:35:40 +0900

    完了TODOが0件の場合は削除ボタンが非活性になるように修正

--- src/views/CompletedTodos.vue
+++ src/views/CompletedTodos.vue
@@ -19,7 +19,7 @@
     <button
       @click="isConfirmModalOpen = true"
       :disabled="completedTodos.length === 0"
-      class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 text-sm"
+      class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 disabled:opacity-50 disabled:cursor-not-allowed"
     >
       完了TODOをすべて削除
     </button>
